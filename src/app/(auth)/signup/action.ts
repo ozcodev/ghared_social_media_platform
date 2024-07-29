ad2ea30708b3c5d446f197124a18614b305fb4fc
Badrozgar <43305158+ozcodev@users.noreply.github.com>
2024-07-29 01:01:34 +0100

    implementation of signup logic

---
+++ src/app/(auth)/signup/action.ts
@@ -0,0 +1,79 @@
+"user server";
+import { lucia } from "@/Auth";
+import prisma from "@/lib/prisma";
+import { signUpShema, signUpValues } from "@/lib/validation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signUp(
+  credentials: signUpValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, password, email } = signUpShema.parse(credentials);
+    const passwordHash = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+    const userId = generateIdFromEntropySize(10);
+
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return {
+        error: "Username already taken",
+      };
+    }
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return {
+        error: "email already taken",
+      };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.log(error);
+    return {
+      error: "Something went wrong",
+    };
+  }
+}
