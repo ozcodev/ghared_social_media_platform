521c5597895a743de7168488865a8aeeb8d61a80
Badrozgar <43305158+ozcodev@users.noreply.github.com>
2024-07-29 01:00:39 +0100

    implement back-end login

---
+++ src/app/(auth)/login/action.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { loginShema, loginValues } from "@/lib/validation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { verify } from "@node-rs/argon2";
+import { lucia } from "@/Auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function login(
+  credentials: loginValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, password } = loginShema.parse(credentials);
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (!existingUser || !existingUser.passwordHash) {
+      return {
+        error: "Incorrect username or password",
+      };
+    }
+
+    const validPassword = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+
+    if (!validPassword) {
+      return {
+        error: "Incorrect username or password",
+      };
+    }
+
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return {
+      error: "Something went wrong",
+    };
+  }
+}
